8f1001e828756ba1815a021281552d64731aee9b
kimsia <kimcity@gmail.com>
2011-11-14 08:54:56 +0800

    add in the db settings for cloudcontrol hosting

--- db.php
+++ db.php
@@ -2,11 +2,17 @@
 
 
 // Database Settings
-
-$user = "root";
-$password = "root";
-$host = "localhost";
-$database_name = "clickbuypay";
+if (strpos($_SERVER['SERVER_NAME'], 'cloudcontrolled.com') !== false) {
+	$user = "depuc9g4bmj";
+	$password = "pQveTyb8H3AgjM6";
+	$host = "127.0.0.1";
+	$database_name = "depuc9g4bmj";
+} else {
+	$user = "root";
+	$password = "root";
+	$host = "localhost";
+	$database_name = "clickbuypay";
+}
 
 
 
