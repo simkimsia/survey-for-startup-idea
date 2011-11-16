ca2bc18b8dfd6ca3272f88795e5477313758ef24
kimsia <kimcity@gmail.com>
2011-11-16 15:32:40 +0800

    production button image requires a id attribute-value

--- db.php
+++ db.php
@@ -14,7 +14,7 @@ $localhostButton = '<form action="https://www.sandbox.paypal.com/cgi-bin/webscr"
 $productionButton = '<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
 <input type="hidden" name="cmd" value="_s-xclick">
 <input type="hidden" name="hosted_button_id" value="E59AY99NG2QF8">
-<input type="image" src="https://www.paypalobjects.com/en_GB/SG/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal — The safer, easier way to pay online.">
+<input type="image" id="paypal-box" src="https://www.paypalobjects.com/en_GB/SG/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal — The safer, easier way to pay online.">
 <img alt="" border="0" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" width="1" height="1">
 </form>';
 
